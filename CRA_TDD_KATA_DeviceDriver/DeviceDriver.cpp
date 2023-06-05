352a57cc8034fdca975f17a5e2f7386b2ef8e9ec
jinhwan.oh <ognan98@gmail.com>
2023-06-05 15:25:02 +0900

    read 기능 구현 및 UT추가

--- CRA_TDD_KATA_DeviceDriver/DeviceDriver.cpp
+++ CRA_TDD_KATA_DeviceDriver/DeviceDriver.cpp
@@ -1,12 +1,20 @@
 #include "DeviceDriver.h"
+#include <windows.h>
 
-DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
+DeviceDriver::DeviceDriver(FlashMemoryDevice *hardware) : m_hardware(hardware)
 {}
 
 int DeviceDriver::read(long address)
 {
-    // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    unsigned char prevCh = m_hardware->read(address);
+    for(int i = 1; i < READ_TRY_CNT; i++)
+    {
+		Sleep(READ_SLEEP_TIME_MS);
+        unsigned char ch = m_hardware->read(address);
+        if (ch != prevCh) throw ReadFailException("Read Fail");
+        prevCh = ch;
+    }
+    return (int)prevCh;
 }
 
 void DeviceDriver::write(long address, int data)
