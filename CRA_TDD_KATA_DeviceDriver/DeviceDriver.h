95e50d6b79ac36677ec8012b6c6c3a4b11ec816f
jinhwan.oh <ognan98@gmail.com>
2023-06-05 16:17:20 +0900

    write 기능 구현 및 UT 추가

--- CRA_TDD_KATA_DeviceDriver/DeviceDriver.h
+++ CRA_TDD_KATA_DeviceDriver/DeviceDriver.h
@@ -6,18 +6,23 @@ using namespace std;
 class DeviceDriver
 {
 public:
-    const int READ_TRY_CNT = 5;
-    const int READ_SLEEP_TIME_MS = 500;
+    static const int READ_TRY_CNT = 5;
+    static const int READ_SLEEP_TIME_MS = 500;
+    static const int ERASE_DATA = 0xFF;
 
     DeviceDriver(FlashMemoryDevice *hardware);
     int read(long address);
     void write(long address, int data);
 
 
-    class ReadFailException : public exception
-    {
+    class ReadFailException : public exception {
 	    public:
 	        explicit ReadFailException(char const* _Message) : exception(_Message) {}
+    };
+
+    class WriteFailException : public exception {
+    public:
+        explicit WriteFailException(char const* _Message) : exception(_Message) {}
     };
 protected:
     FlashMemoryDevice *m_hardware;
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -4,6 +4,8 @@
 using namespace testing;
 
 #define NOT_IMPORTANT_ADDR (0x00000000)
+#define NOT_IMPORTANT_DATA (0xA)
+#define NOT_IMPORTANT_DATA_2 (0xB)
 
 class MockFlashMemoryDevice : public FlashMemoryDevice
 {
@@ -16,20 +18,50 @@ TEST(DeviceDriverTest, ReadSuccess) {
 	MockFlashMemoryDevice mockFlashMemoryDevice;
 	EXPECT_CALL(mockFlashMemoryDevice, read)
 		.Times(5)
-		.WillRepeatedly(Return('A'));
+		.WillRepeatedly(Return(NOT_IMPORTANT_DATA));
 
 	DeviceDriver deviceDriver(&mockFlashMemoryDevice);
-	EXPECT_THAT(deviceDriver.read(NOT_IMPORTANT_ADDR), Eq('A'));
+	EXPECT_THAT(deviceDriver.read(NOT_IMPORTANT_ADDR), Eq(NOT_IMPORTANT_DATA));
 }
 
 TEST(DeviceDriverTest, ReadFail) {
 	MockFlashMemoryDevice mockFlashMemoryDevice;
 	EXPECT_CALL(mockFlashMemoryDevice, read)
 		.Times(2)
-		.WillOnce(Return('B'))
-		.WillOnce(Return('A'))
-		.WillRepeatedly(Return('B'));
+		.WillOnce(Return(NOT_IMPORTANT_DATA))
+		.WillOnce(Return(NOT_IMPORTANT_DATA_2))
+		.WillRepeatedly(Return(NOT_IMPORTANT_DATA));
 
 	DeviceDriver deviceDriver(&mockFlashMemoryDevice);
 	EXPECT_THROW(deviceDriver.read(NOT_IMPORTANT_ADDR), DeviceDriver::ReadFailException);
+}
+
+TEST(DeviceDriverTest, WriteSuccess) {
+	MockFlashMemoryDevice mockFlashMemoryDevice;
+	EXPECT_CALL(mockFlashMemoryDevice, read)
+		.Times(10)
+		.WillOnce(Return(DeviceDriver::ERASE_DATA))
+		.WillOnce(Return(DeviceDriver::ERASE_DATA))
+		.WillOnce(Return(DeviceDriver::ERASE_DATA))
+		.WillOnce(Return(DeviceDriver::ERASE_DATA))
+		.WillOnce(Return(DeviceDriver::ERASE_DATA))
+		.WillRepeatedly(Return(NOT_IMPORTANT_DATA));
+	EXPECT_CALL(mockFlashMemoryDevice, write)
+		.Times(1);
+
+	DeviceDriver deviceDriver(&mockFlashMemoryDevice);
+	EXPECT_NO_THROW(deviceDriver.write(NOT_IMPORTANT_ADDR, NOT_IMPORTANT_DATA), DeviceDriver::WriteFailException);
+	EXPECT_THAT(deviceDriver.read(NOT_IMPORTANT_ADDR), Eq(NOT_IMPORTANT_DATA));
+}
+
+TEST(DeviceDriverTest, WriteFail) {
+	MockFlashMemoryDevice mockFlashMemoryDevice;
+	EXPECT_CALL(mockFlashMemoryDevice, read)
+		.Times(5)
+		.WillRepeatedly(Return(NOT_IMPORTANT_DATA));
+	EXPECT_CALL(mockFlashMemoryDevice, write)
+		.Times(0);
+
+	DeviceDriver deviceDriver(&mockFlashMemoryDevice);
+	EXPECT_THROW(deviceDriver.write(NOT_IMPORTANT_ADDR, NOT_IMPORTANT_DATA), DeviceDriver::WriteFailException);
 }
